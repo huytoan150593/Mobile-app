78cb8f58718db7973e525889204f3dc0d6ecdc0c
huytoan150593 <mrtoan150593@gmail.com>
2021-06-16 14:12:29 +0900

    update animation for button

--- 01_omikuji/main.js
+++ 01_omikuji/main.js
@@ -2,6 +2,27 @@
 
 window.addEventListener("DOMContentLoaded",
     function(){
+        $("header").textillate({
+            loop: false, // ループのオンオフ
+            minDisplayTime: 2000, // テキストが置き換えられるまでの表示時間
+            initialDelay: 2000, // 遅延時間
+            autoStart: true, // アニメーションを自動的にスタート
+            in: { // フェードインのエフェクトの詳細設定
+            effect: "fadeInLeftBig", // エフェクトの名前(animate.css参照)
+            delayScale: 1.5, // 遅延時間の指数
+            delay: 50, // 文字ごとの遅延時間
+            sync: false, // trueはアニメーションをすべての文字に同時に適用
+            shuffle: true // trueは文字を順番にではなく、ランダムに
+            }
+            });
+            // おみくじボタン(id="btn1") ボヤァと表示させる
+            $(function(){
+            ScrollReveal().reveal("#btn1", { duration: 9000 });
+            });
+    }
+)
+window.addEventListener("DOMContentLoaded",
+    function(){
         let popMessage = "いらっしゃい！おみくじ引いてって";
         window.alert(popMessage);
     }, false
@@ -11,24 +32,28 @@ window.addEventListener("DOMContentLoaded",
 const btn = document.getElementById("btn");
 btn.addEventListener("click",
     function(){
-        let n = Math.floor(Math.random() * 3);
+        let resultText = ["大吉!!!!!", "吉!!!!", "中吉!!!", "小吉!!", "末吉!", "凶。。",];
+        let resultColor = ["#ff0000", "#c71585", "#ff1493", "#ff69b4", "#ff8c00", "#1e90ff"];
+        let resultFontSize = ["55px", "50px", "45px", "40px", "35px", "30px"];
+        let n = Math.floor(Math.random() * resultText.length);
 
-        switch(n){
-            case 0:
-                btn.innerText = "Very Happy!!";
-                btn.style.color = "#ff0000";
-                btn.style.fontSize = "40px";
-                break;
-            case 1:
-                btn.innerText = "Happy!!";
-                btn.style.color = "#fff001";
-                btn.style.fontSize = "30px";
-                break;
-            case 2:
-                btn.innerText = "UnHappy...";
-                btn.style.color = "#261e1c";
-                btn.style.fontSize = "20px";
-                break;
-        }
+        btn.textContent = resultText[n];
+        btn.style.color = resultColor[n];
+        btn.style.fontSize = resultFontSize[n];
+
+            // snowfall stop
+        $(document).snowfall("clear");
+        // jQueryのsnowfall
+        $(document).ready(function(){
+        $(document).snowfall({
+        maxSpeed : 5, // 最大速度
+        minSpeed : 1, // 最小速度
+        maxSize : 20, // 最大サイズ
+        minSize : 1, // 最小サイズ
+        image : 'images/sakura_hanabira.png'
+        });
+        });
+        
     }, false
 );
+
