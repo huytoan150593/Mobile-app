16d011a859b7002e2a02a4a25f1f593b73f7ec19
huytoan150593 <mrtoan150593@gmail.com>
2021-07-01 11:47:00 +0900

    big update

--- 01_omikuji/main.js
+++ 01_omikuji/main.js
@@ -28,43 +28,47 @@ window.addEventListener("DOMContentLoaded",
     }, false
 
 );
-
-const btn = document.getElementById("btn");
-const textButton = document.getElementById("text-btn");
-btn.addEventListener("click",
-    function(){
-        let resultText = ["大吉!!!!!", "吉!!!!", "中吉!!!", "小吉!!", "末吉!", "凶。。",];
-        let resultColor = ["#ff0000", "#c71585", "#ff1493", "#ff69b4", "#ff8c00", "#1e90ff"];
-        let resultFontSize = ["80px", "80px", "70px", "60px", "50px", "40px"];
-        let resultSound = ["sound/omikuji_sound1.mp3", "sound/omikuji_sound2.mp3", "sound/omikuji_sound3.mp3", "sound/omikuji_sound4.mp3", "sound/omikuji_sound5.mp3", "sound/omikuji_sound5.mp3"];
-        let n = Math.floor(Math.random() * resultText.length);
-
-        let resultMaxSpeed = [20,20,15,10,5,5];
-        let resultMaxSize = [30,30,20,15,20,20];
-        let resultImage = ["images/star.png","images/sakura_hanabira.png","images/sakura_hanabira.png","images/sakura_hanabira.png","images/leaf.png","images/snowflakes.png"];
-
-        let music = new Audio(resultSound[n]);
-        music.currentTime = 0;
-        music.play();
-
-        textButton.textContent = resultText[n];
-        textButton.style.color = resultColor[n];
-        textButton.style.fontSize = resultFontSize[n];
-
-            // snowfall stop
-        $(document).snowfall("clear");
-        // jQueryのsnowfall
-
-        $(document).ready(function(){
-        $(document).snowfall({
-        maxSpeed : resultMaxSpeed[n], // 最大速度
-        minSpeed : 1, // 最小速度
-        maxSize : resultMaxSize[n], // 最大サイズ
-        minSize : 1, // 最小サイズ
-        image : resultImage[n]
-        });
-        });
+function Library(options){
+    const btn = document.querySelector(options.button);
+    const textButton = document.querySelector(options.text);
+    btn.addEventListener("click",
+        function(){
+            let resultText = ["大吉!!!!!", "吉!!!!", "中吉!!!", "小吉!!", "末吉!", "凶。。",];
+            let resultColor = ["#ff0000", "#c71585", "#ff1493", "#ff69b4", "#ff8c00", "#1e90ff"];
+            let resultFontSize = ["80px", "80px", "70px", "60px", "50px", "40px"];
+            let resultSound = ["sound/omikuji_sound1.mp3", "sound/omikuji_sound2.mp3", "sound/omikuji_sound3.mp3", "sound/omikuji_sound4.mp3", "sound/omikuji_sound5.mp3", "sound/omikuji_sound5.mp3"];
+            let n = Math.floor(Math.random() * resultText.length);
         
-    }, false
-);
+            let resultMaxSpeed = [20,20,15,10,5,5];
+            let resultMaxSize = [30,30,20,15,20,20];
+            let resultImage = ["images/star.png","images/sakura_hanabira.png","images/sakura_hanabira.png","images/sakura_hanabira.png","images/leaf.png","images/snowflakes.png"];
+        
+            let music = new Audio(resultSound[n]);
+            music.currentTime = 0;
+            music.play();
+            console.log(textButton);
+            textButton.textContent = resultText[n];
+            textButton.style.color = resultColor[n];
+            textButton.style.fontSize = resultFontSize[n];
+        
+                // snowfall stop
+            $(document).snowfall("clear");
+            // jQueryのsnowfall
+        
+            $(document).ready(function(){
+            $(document).snowfall({
+            maxSpeed : resultMaxSpeed[n], // 最大速度
+            minSpeed : 1, // 最小速度
+            maxSize : resultMaxSize[n], // 最大サイズ
+            minSize : 1, // 最小サイズ
+            image : resultImage[n]
+            });
+            });
+    }, false);
+
+};
+console.log(btn);
+
+
+
 
