7a427ace713586c0b88ab0e74da13e7e594f35ca
huytoan150593 <mrtoan150593@gmail.com>
2021-12-13 18:52:18 +0900

    win-lose logic

--- 04_tic tac toe/js/tic_tac_toe.js
+++ 04_tic tac toe/js/tic_tac_toe.js
@@ -3,6 +3,9 @@
 let flag = true;
 let counter = 9;
 
+const squares = document.getElementsByClassName("square");
+const squaresArray = Array.from(squares);
+
 const a_1 = document.querySelector("#a_1");
 const a_2 = document.querySelector("#a_2");
 const a_3 = document.querySelector("#a_3");
@@ -13,9 +16,24 @@ const c_1 = document.querySelector("#c_1");
 const c_2 = document.querySelector("#c_2");
 const c_3 = document.querySelector("#c_3");
 
-const mgstxt1 = '<p class="image"><img src="./img/penguins.jpg" width="61px" height="61px" alt=""></p><p>Peguins Attack!</p>';
-const mgstxt2 = '<p class="image"><img src="./img/whitebear.jpg" width="61px" height="61px" alt=""></p><p>WhiteBear Attack!</p>';
-const mgstxt3 = '<p>DRAW</p>'
+const line1 = JudgLine(squaresArray, ["a_1", "a_2", "a_3"]);
+const line2 = JudgLine(squaresArray, ["b_1", "b_2", "b_3"]);
+const line3 = JudgLine(squaresArray, ["c_1", "c_2", "c_3"]);
+const line4 = JudgLine(squaresArray, ["a_1", "b_1", "c_1"]);
+const line5 = JudgLine(squaresArray, ["a_2", "b_2", "c_2"]);
+const line6 = JudgLine(squaresArray, ["a_3", "b_3", "c_3"]);
+const line7 = JudgLine(squaresArray, ["a_1", "b_2", "c_3"]);
+const line8 = JudgLine(squaresArray, ["a_3", "b_2", "c_1"]);
+console.log(line1);
+console.log(line2);
+const lineArray = [line1, line2, line3, line4, line5, line6, line7, line8];
+let winningLine = null;
+
+const msgtxt1 = '<p class="image"><img src="./img/penguins.jpg" width="61px" height="61px" alt=""></p><p>Peguins Attack!</p>';
+const msgtxt2 = '<p class="image"><img src="./img/whitebear.jpg" width="61px" height="61px" alt=""></p><p>WhiteBear Attack!</p>';
+const msgtxt3 = '<p class="image"><img src="./img/penguins.jpg" width="61px" height="61px"></p><p class="animate_animated animate_lightSpeedInRight">Peguins Win!</p>';
+const msgtxt4 = '<p class="image"><img src="./img/whitebear.jpg" width="61px" height="61px"></p><p class="animate_animated animate_lightSpeedInLeft">WhiteBear Win!</p>';
+const msgtxt5 = '<p class="image"><img src="./img/penguins.jpg" width="61px" height="61px"><img src="./img/whitebear.jpg" width="61px" height="61px"></p><p class="text animate_bounceIn">Draw!!</p>';
 
 window.addEventListener("DOMContentLoaded", 
     function(){
@@ -50,16 +68,28 @@ c_3.addEventListener("click", () => {
     isSelect(c_3);
 });
 
-
+function JudgLine(targetArray, idArray){
+    return targetArray.filter(function(e){
+        return (e.id === idArray[0] || e.id === idArray[1] || e.id === idArray[2]);
+    });
+}
 function isSelect(selectSquare){
     if(flag === true){
         selectSquare.classList.add("js-pen-checked");
         selectSquare.classList.add("js-unclickable");
+        if(isWinner("penguins") === true){
+            setMessage("pen-win");
+            return;
+        }
         setMessage("bear-turn");
         flag = false;
     }else{
         selectSquare.classList.add("js-bear-checked");
         selectSquare.classList.add("js-unclickable");
+        if(isWinner("bear") === true){
+            setMessage("bear-win");
+            return;
+        }
         setMessage("pen-turn");
         flag = true;
     }
@@ -74,14 +104,37 @@ function setMessage(id){
     const node = document.querySelector("#msgtext");
     switch(id){
         case "pen-turn":
-            node.innerHTML = mgstxt1;
+            node.innerHTML = msgtxt1;
             break;
         case "bear-turn":
-            node.innerHTML = mgstxt2;
+            node.innerHTML = msgtxt2;
+            break;
+        case "pen-win":
+            node.innerHTML = msgtxt3;
+            break;
+        case "bear-win":
+            node.innerHTML = msgtxt4;
             break;
         case "draw":
-    
-        default:
+            node.innerHTML = msgtxt5;
             break;
+        default:
+            document.getElementById("msgtext").innerHTML = msgtxt1;
     }
+}
+
+function isWinner(symbol){
+    const result = lineArray.some(function(line){
+        const subResult = line.every(function(square){
+            if(symbol === "penguins"){
+                return square.classList.contains("js-pen-checked");
+            }
+            if(symbol === "bear"){
+                return square.classList.contains("js-bear-checked");
+            }
+        });
+        if(subResult){ winningLine = line}
+        return subResult;
+    });
+    return result;
 }
